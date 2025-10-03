4deb9ff599aa167a903758f74d2b0cb33d074600
Ezra Abarca <ezra.alejandro.abarca.cordova@gmail.com>
2025-10-02 22:37:05 -0600

    feat: batch submit torrents

---
+++ submit.js
@@ -0,0 +1,43 @@
+import fs from 'fs';
+
+const files = fs.readdirSync('./out')
+console.log(files)
+
+const names = files.map(file => {
+    return file.split('.')[0]
+})
+const ids = [...new Set(names)]
+console.log(ids)
+
+for (const id of ids) {
+    // check all files exist?
+    const metaExists = fs.existsSync(`./out/${id}.meta.txt`)
+    const videoExists = fs.existsSync(`./out/${id}.mp4`)
+    const torrentExists = fs.existsSync(`./out/${id}.torrent`)
+    if(metaExists & videoExists & torrentExists) {
+        // load duration
+        const duration = fs.readFileSync(`./out/${id}.meta.txt`, 'utf8');
+        console.log(duration)
+        // load torrent file
+        const torrent = fs.readFileSync(`./out/${id}.torrent`, 'base64');
+        console.log(torrent)
+
+        // post to api
+        const response = await fetch(process.env.api_url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json", // important!
+                "Authorization": "Bearer devtoken123",
+            },
+            body: JSON.stringify({
+                torrent: torrent,
+                duration: duration,
+                id: id,
+            }),
+        });
+        console.log(response.statusText);
+    }
+    else{
+        console.log('missing files for ', id)
+    }
+}
